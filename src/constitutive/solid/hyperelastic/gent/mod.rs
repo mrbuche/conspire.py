7647a5f30264170610a1c388798a8f7976fa90de
mrbuche <dev@mrbuche.com>
2025-01-01 18:46:49 -0700

    asdf

--- src/constitutive/solid/hyperelastic/gent/mod.rs
+++ src/constitutive/solid/hyperelastic/gent/mod.rs
@@ -1,4 +1,50 @@
+use crate::PyErrGlue;
+use conspire::{
+    constitutive::{
+        solid::{elastic::Elastic, hyperelastic::Gent as GentConspire},
+        Constitutive,
+    },
+    math::TensorArray,
+    mechanics::DeformationGradient,
+};
+use numpy::PyArray2;
 use pyo3::prelude::*;
 
 #[pyclass]
-pub struct Gent {}
+pub struct Gent {
+    bulk_modulus: f64,
+    shear_modulus: f64,
+    extensibility: f64,
+}
+
+#[pymethods]
+impl Gent {
+    #[new]
+    fn new(bulk_modulus: f64, shear_modulus: f64, extensibility: f64) -> Self {
+        Self {
+            bulk_modulus,
+            shear_modulus,
+            extensibility,
+        }
+    }
+    fn helmholtz_free_energy_density<'py>(
+        &self,
+        py: Python<'py>,
+    ) -> Result<Bound<'py, PyArray2<f64>>, PyErrGlue> {
+        Ok(PyArray2::from_vec2(
+            py,
+            &GentConspire::new(&[self.bulk_modulus, self.shear_modulus, self.extensibility])
+                .calculate_cauchy_stress(&DeformationGradient::new([
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                ]))?
+                .as_array()
+                .iter()
+                .map(|entry| entry.to_vec())
+                .collect::<Vec<Vec<f64>>>()
+                .to_vec(),
+        )
+        .unwrap())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod constitutive;
 
-use pyo3::prelude::*;
+use ::conspire::constitutive::ConstitutiveError;
+use pyo3::{exceptions::PyTypeError, prelude::*};
 
 #[pymodule]
 fn conspire(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -8,4 +9,22 @@ fn conspire(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     py.import("sys")?
         .getattr("modules")?
         .set_item("conspire.constitutive", m)
+}
+
+pub struct PyErrGlue {
+    message: String,
+}
+
+impl From<PyErrGlue> for PyErr {
+    fn from(error: PyErrGlue) -> Self {
+        PyTypeError::new_err(error.message)
+    }
+}
+
+impl From<ConstitutiveError> for PyErrGlue {
+    fn from(error: ConstitutiveError) -> Self {
+        PyErrGlue {
+            message: format!("{:?}\x1B[A", error),
+        }
+    }
 }
