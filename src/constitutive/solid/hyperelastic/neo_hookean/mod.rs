5aefaa1fae2d4fc3f8d336e76908caef7a32dcb8
mrbuche <dev@mrbuche.com>
2025-04-10 16:05:04 -0600

    uv

--- src/constitutive/solid/hyperelastic/neo_hookean/mod.rs
+++ src/constitutive/solid/hyperelastic/neo_hookean/mod.rs
@@ -1,10 +1,10 @@
 use crate::PyErrGlue;
 use conspire::constitutive::{
+    Constitutive,
     solid::{
         elastic::Elastic,
         hyperelastic::{Hyperelastic, NeoHookean as NeoHookeanConspire},
     },
-    Constitutive,
 };
 use ndarray::Array;
 use numpy::{PyArray2, PyArray4};
