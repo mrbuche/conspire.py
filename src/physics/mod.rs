f42a6accc7ff0ca4a0cccc4abec2013e5719783d
mrbuche <dev@mrbuche.com>
2026-04-01 21:07:47 -0600

    most of em

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -2,13 +2,24 @@ use crate::PyErrGlue;
 use conspire::{
     math::Scalar,
     physics::molecular::single_chain::{
-        Ensemble, FreelyJointedChain as Inner, SingleChainError, Thermodynamics,
+        // ArbitraryPotentialFreelyJointedChain as Ufjc,
+        Ensemble,
+        ExtensibleFreelyJointedChain as Efjc,
+        FreelyJointedChain as Fjc,
+        IdealChain as Ideal,
+        SingleChainError,
+        SquareWellFreelyJointedChain as Swfjc,
+        Thermodynamics,
     },
 };
 use pyo3::prelude::*;
 
 pub fn register_module(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<FreelyJointedChain>()
+    // m.add_class::<ArbitraryPotentialFreelyJointedChain>()?;
+    m.add_class::<ExtensibleFreelyJointedChain>()?;
+    m.add_class::<FreelyJointedChain>()?;
+    m.add_class::<IdealChain>()?;
+    m.add_class::<SquareWellFreelyJointedChain>()
 }
 
 impl From<SingleChainError> for PyErrGlue {
@@ -19,90 +30,103 @@ impl From<SingleChainError> for PyErrGlue {
     }
 }
 
-#[pyclass]
-pub struct FreelyJointedChain(Inner);
-
-#[pymethods]
-impl FreelyJointedChain {
-    #[new]
-    fn new(
-        ensemble: String,
-        link_length: Scalar,
-        number_of_links: u8,
-        temperature: Scalar,
-    ) -> Self {
-        let ensemble = match ensemble.as_str() {
-            "isometric" => Ensemble::Isometric(temperature),
-            "isotensional" => Ensemble::Isotensional(temperature),
-            _ => panic!(),
-        };
-        Self(Inner {
-            link_length,
-            number_of_links,
-            ensemble,
-        })
-    }
-    fn nondimensional_helmholtz_free_energy(
-        &self,
-        nondimensional_extension: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self
-            .0
-            .nondimensional_helmholtz_free_energy(nondimensional_extension)?)
-    }
-    fn nondimensional_helmholtz_free_energy_per_link(
-        &self,
-        nondimensional_extension: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self
-            .0
-            .nondimensional_helmholtz_free_energy_per_link(nondimensional_extension)?)
-    }
-    fn nondimensional_force(&self, nondimensional_extension: Scalar) -> Result<Scalar, PyErrGlue> {
-        Ok(self.0.nondimensional_force(nondimensional_extension)?)
-    }
-    fn nondimensional_stiffness(
-        &self,
-        nondimensional_extension: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self.0.nondimensional_stiffness(nondimensional_extension)?)
-    }
-    fn nondimensional_radial_distribution(
-        &self,
-        nondimensional_extension: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self
-            .0
-            .nondimensional_radial_distribution(nondimensional_extension)?)
-    }
-    fn nondimensional_spherical_distribution(
-        &self,
-        nondimensional_extension: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self
-            .0
-            .nondimensional_spherical_distribution(nondimensional_extension)?)
-    }
-    fn nondimensional_gibbs_free_energy(
-        &self,
-        nondimensional_force: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self
-            .0
-            .nondimensional_gibbs_free_energy(nondimensional_force)?)
-    }
-    fn nondimensional_gibbs_free_energy_per_link(
-        &self,
-        nondimensional_force: Scalar,
-    ) -> Result<Scalar, PyErrGlue> {
-        Ok(self
-            .0
-            .nondimensional_gibbs_free_energy_per_link(nondimensional_force)?)
-    }
-    fn nondimensional_extension(&self, nondimensional_force: Scalar) -> Result<Scalar, PyErrGlue> {
-        Ok(self.0.nondimensional_extension(nondimensional_force)?)
-    }
-    fn nondimensional_compliance(&self, nondimensional_force: Scalar) -> Result<Scalar, PyErrGlue> {
-        Ok(self.0.nondimensional_compliance(nondimensional_force)?)
-    }
+macro_rules! single_chain {
+    ($model:ident, $inner:ident, $($parameter: ident),+ $(,)?) => {
+        #[pyclass]
+        pub struct $model($inner);
+        #[pymethods]
+        impl $model {
+            #[new]
+            fn new(
+                number_of_links: u8,
+                $($parameter: Scalar),+,
+                ensemble: String,
+                temperature: Scalar,
+            ) -> Self {
+                let ensemble = match ensemble.as_str() {
+                    "isometric" => Ensemble::Isometric(temperature),
+                    "isotensional" => Ensemble::Isotensional(temperature),
+                    _ => panic!(),
+                };
+                Self($inner {
+                    number_of_links,
+                    $($parameter),+,
+                    ensemble,
+                })
+            }
+            fn nondimensional_helmholtz_free_energy(
+                &self,
+                nondimensional_extension: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self
+                    .0
+                    .nondimensional_helmholtz_free_energy(nondimensional_extension)?)
+            }
+            fn nondimensional_helmholtz_free_energy_per_link(
+                &self,
+                nondimensional_extension: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self
+                    .0
+                    .nondimensional_helmholtz_free_energy_per_link(nondimensional_extension)?)
+            }
+            fn nondimensional_force(&self, nondimensional_extension: Scalar) -> Result<Scalar, PyErrGlue> {
+                Ok(self.0.nondimensional_force(nondimensional_extension)?)
+            }
+            fn nondimensional_stiffness(
+                &self,
+                nondimensional_extension: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self.0.nondimensional_stiffness(nondimensional_extension)?)
+            }
+            fn nondimensional_radial_distribution(
+                &self,
+                nondimensional_extension: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self
+                    .0
+                    .nondimensional_radial_distribution(nondimensional_extension)?)
+            }
+            fn nondimensional_spherical_distribution(
+                &self,
+                nondimensional_extension: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self
+                    .0
+                    .nondimensional_spherical_distribution(nondimensional_extension)?)
+            }
+            fn nondimensional_gibbs_free_energy(
+                &self,
+                nondimensional_force: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self
+                    .0
+                    .nondimensional_gibbs_free_energy(nondimensional_force)?)
+            }
+            fn nondimensional_gibbs_free_energy_per_link(
+                &self,
+                nondimensional_force: Scalar,
+            ) -> Result<Scalar, PyErrGlue> {
+                Ok(self
+                    .0
+                    .nondimensional_gibbs_free_energy_per_link(nondimensional_force)?)
+            }
+            fn nondimensional_extension(&self, nondimensional_force: Scalar) -> Result<Scalar, PyErrGlue> {
+                Ok(self.0.nondimensional_extension(nondimensional_force)?)
+            }
+            fn nondimensional_compliance(&self, nondimensional_force: Scalar) -> Result<Scalar, PyErrGlue> {
+                Ok(self.0.nondimensional_compliance(nondimensional_force)?)
+            }
+        }
+    };
 }
+
+single_chain!(
+    ExtensibleFreelyJointedChain,
+    Efjc,
+    link_length,
+    link_stiffness
+);
+single_chain!(IdealChain, Ideal, link_length);
+single_chain!(FreelyJointedChain, Fjc, link_length);
+single_chain!(SquareWellFreelyJointedChain, Swfjc, link_length, well_width);
